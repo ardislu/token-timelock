509174daf462d3e6af653194e19c4ae83f9d4103
Ardis Lu <ardisglu@gmail.com>
2023-06-19 21:57:47 -0700

    Use precise -Depth parameter
    Not required for ConvertFrom, but it is for ConvertTo

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
-$output = solc --standard-json ./input.json | ConvertFrom-Json -Depth 100
+$output = solc --standard-json ./input.json | ConvertFrom-Json
 $output.contracts.'Timelock.sol'.Timelock.evm.bytecode.object | Out-File ./dist/Timelock.bin
 $metadata = $output.contracts.'Timelock.sol'.Timelock.metadata
 $metadata | Out-File ./dist/Timelock-metadata.json
-$newInput = Get-Content ./input.json | ConvertFrom-Json -Depth 100
-$newInput.sources = ($metadata | ConvertFrom-Json -Depth 100).sources
-$newInput | ConvertTo-Json -Depth 100 | Out-File ./dist/Timelock-input.json
+$newInput = Get-Content ./input.json | ConvertFrom-Json
+$newInput.sources = ($metadata | ConvertFrom-Json).sources
+$newInput | ConvertTo-Json -Depth 5 | Out-File ./dist/Timelock-input.json
