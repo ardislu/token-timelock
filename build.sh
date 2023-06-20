29442fa08289dcc2b17028df8db6a59c633d8db4
Ardis Lu <ardisglu@gmail.com>
2023-06-19 22:34:47 -0700

    Use string literals for all solc output data
    Prevents spaces in solc output from being collapsed

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-output=$(solc --standard-json ./input.json)
-echo $output | jq -j '.contracts."Timelock.sol".Timelock.evm.bytecode.object' > './dist/Timelock.bin'
-metadata=$(echo $output | jq -r '.contracts."Timelock.sol".Timelock.metadata')
-echo -n $metadata > './dist/Timelock-metadata.json'
+output="$(solc --standard-json ./input.json)"
+echo "$output" | jq -j '.contracts."Timelock.sol".Timelock.evm.bytecode.object' > './dist/Timelock.bin'
+metadata=$(echo "$output" | jq -r '.contracts."Timelock.sol".Timelock.metadata')
+echo -n "$metadata" > './dist/Timelock-metadata.json'
 newInput=$(cat ./input.json)
-echo "$newInput" | jq -j --argjson newSources "$(echo $metadata | jq '.sources')" '.sources = $newSources' > './dist/Timelock-input.json'
+echo "$newInput" | jq -j --argjson newSources "$(echo "$metadata" | jq '.sources')" '.sources = $newSources' > './dist/Timelock-input.json'
