3ba0db115edf37005160fe52be7512ae871aaa08
Ardis Lu <ardisglu@gmail.com>
2023-06-19 22:20:27 -0700

    Remove trailing newline from outputs

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $output = solc --standard-json ./input.json | ConvertFrom-Json
-$output.contracts.'Timelock.sol'.Timelock.evm.bytecode.object | Out-File ./dist/Timelock.bin
+$output.contracts.'Timelock.sol'.Timelock.evm.bytecode.object | Out-File -NoNewLine ./dist/Timelock.bin
 $metadata = $output.contracts.'Timelock.sol'.Timelock.metadata
-$metadata | Out-File ./dist/Timelock-metadata.json
+$metadata | Out-File -NoNewLine ./dist/Timelock-metadata.json
 $newInput = Get-Content ./input.json | ConvertFrom-Json
 $newInput.sources = ($metadata | ConvertFrom-Json).sources
-$newInput | ConvertTo-Json -Depth 5 | Out-File ./dist/Timelock-input.json
+$newInput | ConvertTo-Json -Depth 5 | Out-File -NoNewLine ./dist/Timelock-input.json
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 output=$(solc --standard-json ./input.json)
-echo $output | jq -r '.contracts."Timelock.sol".Timelock.evm.bytecode.object' > './dist/Timelock.bin'
+echo $output | jq -j '.contracts."Timelock.sol".Timelock.evm.bytecode.object' > './dist/Timelock.bin'
 metadata=$(echo $output | jq -r '.contracts."Timelock.sol".Timelock.metadata')
-echo $metadata > './dist/Timelock-metadata.json'
+echo -n $metadata > './dist/Timelock-metadata.json'
 newInput=$(cat ./input.json)
-echo "$newInput" | jq --argjson newSources "$(echo $metadata | jq '.sources')" '.sources = $newSources' > './dist/Timelock-input.json'
+echo "$newInput" | jq -j --argjson newSources "$(echo $metadata | jq '.sources')" '.sources = $newSources' > './dist/Timelock-input.json'
